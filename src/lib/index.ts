ae475faf84dbcbbdeb08021a390614ad9f46aa2f
Ethan Dawes <ethand.python@gmail.com>
2025-05-03 18:19:11 -0400

    Import existing work

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,10 @@
-// Reexport your entry components here
+/** Makes a list of numbers from start (inclusive) to stop (exclusive) in `step` increments */
+export function range(start: number, stop: number, step = 1) {
+    // pretty nieve implementation but idk
+    return [...Array(Math.ceil((stop - start) / step)).keys()].map(i => i * step + start)
+}
+
+export * from "./units.js"
+export * from "./timeutils.js"
+export { default as Availability } from "./Availability.svelte"
+export { default as ManualInput } from "./ManualInput.svelte"
